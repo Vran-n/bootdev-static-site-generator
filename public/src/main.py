6b1e22456053b77b3522e2a95ad518a77d995a98
Vran-n <themincraftplayer2474@gmail.com>
2026-08-10 18:39:04 +0800

    reorganized file structure

--- public/src/main.py
+++
@@ -1,4 +0,0 @@
-from textnode import TextNode
-
-print(TextNode("test", "link", "https://youtube.com"))
-print(TextNode("test", "link", "https://youtube.com") == TextNode("test", "link", "https://youtube.com"))
